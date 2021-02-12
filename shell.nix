75913f906d431f283792dff98996a066b869e29f
Yurii Rashkovskii <yrashk@gmail.com>
2021-02-12 13:28:33 -0800

    Problem: lack of specificity about required Rust version
    Solution: specify that it should run on stable
    
    Also, expose this baseline through shell.nix and rust-toolchain
    
    CI tests now explicitly include 1.50.0

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustChannel = (pkgs.rustChannelOf { channel = "stable";  });
+  rust = (rustChannel.rust.override {
+    targets = ["wasm32-unknown-unknown"];
+  });
+in
+pkgs.stdenv.mkDerivation rec {
+  name = "wasm-rs-async-channel-shell";
+
+  buildInputs = with pkgs; [ rust nodejs
+                             openssl.dev pkgconfig # for cargo-release
+  ];
+
+  shellHook = ''
+    # Useful for ensuring cargo tools are available (like cargo-do, for example)
+    export PATH=$PATH:$HOME/.cargo/bin
+  '';
+
+}
